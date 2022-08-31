fe4e3e95ddfae43caf29568ad1c9b23fb6356d19
tabish748 <irfantabish719@gmail.com>
2022-08-31 12:41:54 +0500

    new initial commit cc

--- src/redux/actions/deleteTransportPackage.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const deleteTransportPackage = (id) => async (dispatch) =>{
-    const response = await axios.delete(`http://localhost:5000/transport/package/delete/${id}`);
-    dispatch({
-        type:'DELETE_TRANSPORT_PACKAGE',
-        payload:response.message
-    })
-}
--- src/redux/actions/getAllHotels.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-export const getAllHotels = () =>async (dispatch) =>{
-    const response = await axios.get("http://localhost:5000/hotels")
-    dispatch({
-        type:'GET_HOTELS',
-        payload:response.data
-    })
-}
